c4174ec3facfd4772dc49595780952559d9492e4
G-regory <carriel.g.ca@gmail.com>
2019-02-22 00:15:24 +0100

    add class CustomDialog.java for popup dialog allow to get comment

---
+++ app/src/main/java/com/carriel/gregory/moodtracker/controler/CustumDialog.java
@@ -0,0 +1,57 @@
+package com.carriel.gregory.moodtracker.controler;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.carriel.gregory.moodtracker.R;
+
+public class CustumDialog extends Dialog {
+    //field
+    private String title;
+    private String subTitle;
+    private Button mOkButton;
+    private Button mCancelButton;
+    private TextView mTextViewTitle;
+    private EditText mEditTextSubTitle;
+
+    public CustumDialog(Activity activity){
+        super(activity, R.style.Theme_AppCompat_Dialog_Alert);
+        setContentView(R.layout.my_note_layout);
+        mOkButton= findViewById(R.id.mynote_ok_btn);
+        mCancelButton= findViewById(R.id.mynote_cancel_btn);
+        mTextViewTitle= findViewById(R.id.mynote_title_txt);
+        mEditTextSubTitle = findViewById(R.id.mynote_subtitle_txt);
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setSubTitle(String subTitle) {
+        this.subTitle = subTitle;
+    }
+
+    public void setEditTextSubTitle(String textSubTitle) {
+        mEditTextSubTitle.setText(textSubTitle);
+    }
+
+    public EditText getEditTextSubTitle() {
+        return mEditTextSubTitle;
+    }
+
+    public Button getOkButton() {
+        return mOkButton;
+    }
+
+    public Button getCancelButton() {
+        return mCancelButton;
+    }
+
+    public void build(){
+        show();
+        mTextViewTitle.setText("Commentaire");
+    }
+}
--- app/src/main/java/com/carriel/gregory/moodtracker/controler/MainActivity.java
+++ app/src/main/java/com/carriel/gregory/moodtracker/controler/MainActivity.java
@@ -5,16 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.carriel.gregory.moodtracker.R;
 
 public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
+    private final String TAG= "MessageMood:Main";
+    private static String mood ="";
+    private static String comment ="";
+
     //*********Views**************
     private ImageView mImageViewSmiley;
     private ConstraintLayout mConstraintLayout;
     private GestureDetector mGestureDetector;
+    private CustumDialog mCustumDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +35,49 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
     private void init() {
         mImageViewSmiley=findViewById(R.id.activity_main_smiley_img);
         mConstraintLayout=findViewById(R.id.activity_main_layout);
+        mGestureDetector= new GestureDetector(this,this);
+        mCustumDialog= new CustumDialog(this);
+    }
+
+    /**
+     * show popup dialog on clic buttonNote
+     */
+    public void buttonNote(View view) {
+        mCustumDialog.setTitle("Commentaire"); //title of popup
+
+        /**
+         * events validate button
+         * record comment, mood and date, show msg toast and hide popup
+         */
+        mCustumDialog.getOkButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //*****keep comment***********
+                comment = mCustumDialog.getEditTextSubTitle().getText().toString();
+                //******hide popup*************
+                mCustumDialog.hide();
+
+
+                //***affiche msg Toat*********
+                Toast.makeText(getApplicationContext(),"Commentaire enregistré",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        /**
+         * event clic button cancel
+         * show msg toast and close popup
+         */
+        mCustumDialog.getCancelButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Commentaire annulé",Toast.LENGTH_LONG).show();
+                //***close popup*******
+                mCustumDialog.cancel();
+            }
+        });
+        //start screen popup
+        mCustumDialog.build();
     }
 
     @Override
