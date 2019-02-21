6687ea0227fc18df9d47f59abffe3bc37f61ba5b
G-regory <carriel.g.ca@gmail.com>
2019-02-22 00:08:31 +0100

    init widget and architecture MVC

--- app/src/main/java/com/carriel/gregory/moodtracker/controler/MainActivity.java
+++ app/src/main/java/com/carriel/gregory/moodtracker/controler/MainActivity.java
@@ -1,16 +1,32 @@
-package com.carriel.gregory.moodtracker;
+package com.carriel.gregory.moodtracker.controler;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.widget.ImageView;
+
+import com.carriel.gregory.moodtracker.R;
 
 public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
+
+    //*********Views**************
+    private ImageView mImageViewSmiley;
+    private ConstraintLayout mConstraintLayout;
+    private GestureDetector mGestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+    }
+
+    private void init() {
+        mImageViewSmiley=findViewById(R.id.activity_main_smiley_img);
+        mConstraintLayout=findViewById(R.id.activity_main_layout);
     }
 
     @Override
